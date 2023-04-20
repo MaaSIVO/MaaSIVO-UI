a69cd7c0b163b7821f59ffea43bc23fda901af5f
AnaidOrtega <itzel.ortega9819@hotmail.com>
2023-04-19 22:18:13 -0500

    CONFIG-1: PostCSS Config and NODE 18

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,7 +6,8 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
-    '@storybook/preset-scss'
+    '@storybook/preset-scss',
+    '@storybook/addon-postcss'
   ],
   framework: '@storybook/react',
   typescript: { reactDocgen: false }
