fb138f7e9cefbacc75ec4dce2537f7d22c314e00
AnaidOrtega <itzel.ortega9819@hotmail.com>
2023-07-03 20:36:10 -0600

    CONFIG-4: Vite Build + Fix Carousel Error

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,5 +1,5 @@
 import { Anaid, Hector, Paulo, Rafa } from '../assets/img';
-import { TeamMemberProps } from '../components';
+import { RoundedCardProps, TeamMemberProps } from '../components';
 import { IconOptions } from '../types/icons';
 
 export interface SocialMediaProps {
@@ -50,3 +50,36 @@ export const teamMemberData = [
       'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
   }
 ] as TeamMemberProps[];
+
+export const values = [
+  {
+    title: 'Excellence',
+    icon: {
+      name: 'Work'
+    }
+  },
+  {
+    title: 'Sustentabilidad',
+    icon: {
+      name: 'Sustentabillity'
+    }
+  },
+  {
+    title: 'Innovación',
+    icon: {
+      name: 'Idea2'
+    }
+  },
+  {
+    title: 'Pasión',
+    icon: {
+      name: 'Heart'
+    }
+  },
+  {
+    title: 'Colaboración',
+    icon: {
+      name: 'Colaboration'
+    }
+  }
+] as RoundedCardProps[];
